6eba8589e117625cab685f7dc9869accdd38823b
Wynand Pieterse <wpieterse@ghostknightstudios.com>
2014-05-25 20:52:19 +0200

    Updated orleans server assembly version file.

--- Source/OrleansHost/Properties/AssemblyInfo.cs
+++ Source/OrleansHost/Properties/AssemblyInfo.cs
@@ -2,35 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("OrleansHost")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SharpBattle.net - Orleans Server")]
+[assembly: AssemblyDescription("Hosts the various grains that SharpBattleNet uses in a single development silo of Orleans")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OrleansHost")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Reaper Interactive")]
+[assembly: AssemblyProduct("SharpBattle.net")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("29f13048-db12-4549-8a0e-1160e8f4ee62")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("1B537BBC-BA60-4EEC-A3A7-E971C0A266B0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]

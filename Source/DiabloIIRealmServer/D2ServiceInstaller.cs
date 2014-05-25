2788fdf40051dc8eace65d198da24d1b1db1904e
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-26 00:35:06 +0200

    Added so that D2RS can start as a Windows service.

---
+++ Source/DiabloIIRealmServer/D2ServiceInstaller.cs
@@ -0,0 +1,36 @@
+﻿namespace Reaper.SharpBattleNet.DiabloIIRealmServer
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.ComponentModel;
+    using System.ServiceProcess;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Configuration;
+    using System.Configuration.Install;
+
+    [RunInstaller(runInstaller: true)]
+    public class D2RSServiceIntaller : Installer
+    {
+        public D2RSServiceIntaller()
+        {
+            var processInstaller = new ServiceProcessInstaller();
+            var serviceInstaller = new ServiceInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalService;
+
+            serviceInstaller.DisplayName = "SharpBattleNet - Diablo II Realm Server";
+            serviceInstaller.StartType = ServiceStartMode.Manual;
+
+            serviceInstaller.ServiceName = "D2RSService";
+
+            this.Installers.Add(processInstaller);
+            this.Installers.Add(serviceInstaller);
+
+            return;
+        }
+    }
+}

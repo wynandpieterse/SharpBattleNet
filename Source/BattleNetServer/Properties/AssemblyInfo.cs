12ace1e8083e2e9943ab0b796bf3a7156a1faf61
Wynand Pieterse <wpieterse@ghostknightstudios.com>
2014-05-25 18:40:07 +0200

    Updated the rest of the assembly configuration for BNS program.

--- Source/BattleNetServer/Properties/AssemblyInfo.cs
+++ Source/BattleNetServer/Properties/AssemblyInfo.cs
@@ -2,15 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("BattleNetServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SharpBattle.net Server")]
+[assembly: AssemblyDescription("This is the main server executable that needs to be run to host most of the services of Battle.net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BattleNetServer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Reaper Interactive")]
+[assembly: AssemblyProduct("SharpBattle.net")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("15cfeb6c-439d-4da4-9abe-f50568be5306")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]

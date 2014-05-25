c1cfc3a9269e8396c12d172994089625cf7d5982
Wynand Pieterse <wpieterse@ghostknightstudios.com>
2014-05-25 18:47:17 +0200

    Updated assembly configuration to host details for D2RS.

--- Source/DiabloIIRealmServer/Properties/AssemblyInfo.cs
+++ Source/DiabloIIRealmServer/Properties/AssemblyInfo.cs
@@ -2,35 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("DiabloIIRealmServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SharpBattle.net - Diablo II Realm Server")]
+[assembly: AssemblyDescription("Hosts a realm server for Diablo II on SharpBattle.net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DiabloIIRealmServer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Reaper Interactive")]
+[assembly: AssemblyProduct("SharpBattle.net")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e18ce923-30fe-4939-a5b8-a45513ceac53")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("3E1BE83B-484F-44AC-8915-0DB613DCBAB6")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]

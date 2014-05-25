5940321033dd1876085d7358c8defc19038edf3b
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-26 00:32:32 +0200

    Added service initialization and basic setup for main server.

---
+++ Source/BattleNetServer/BNSService.cs
@@ -0,0 +1,28 @@
+﻿namespace Reaper.SharpBattleNet.BattleNetServer
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.ServiceProcess;
+
+    internal class BNSService : ServiceBase
+    {
+        protected override void OnStart(string[] args)
+        {
+            base.OnStart(args);
+
+            return;
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+
+            return;
+        }
+    }
+}

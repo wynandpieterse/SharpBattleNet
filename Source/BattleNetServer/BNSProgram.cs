3a94638570854205a2808198e7975491960992a5
Wynand Pieterse <wpieterse@ghostknightstudios.com>
2014-05-25 18:28:54 +0200

    Updated entry program to compile. BNS initial startup complete.

---
+++ Source/BattleNetServer/BNSProgram.cs
@@ -0,0 +1,52 @@
+﻿namespace Reaper.SharpBattleNet.BattleNetServer
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal static sealed class BNSProgramReaper
+    {
+        private static void Setup()
+        {
+            Console.Title = "SharpBattleNet Server";
+            Console.WindowWidth = 120;
+            Console.WindowHeight = 40;
+
+            return;
+        }
+
+        private static void Pause()
+        {
+            bool enterPressed = false;
+            ConsoleKeyInfo lastKey = default(ConsoleKeyInfo);
+
+            Console.WriteLine("Press ENTER to continue...");
+
+            while(false == enterPressed)
+            {
+                if(true == Console.KeyAvailable)
+                {
+                    lastKey = Console.ReadKey(true);
+                    if(lastKey.Key == ConsoleKey.Enter)
+                    {
+                        enterPressed = true;
+                    }
+                }
+            }
+
+            return;
+        }
+
+        private static void Main(string[] args)
+        {
+            Setup();
+
+            Pause();
+            return;
+        }
+    }
+}

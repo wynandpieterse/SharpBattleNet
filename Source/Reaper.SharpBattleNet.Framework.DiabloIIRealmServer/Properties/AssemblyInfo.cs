7b5b3921ba78d40a0647e8cf07770ddceb493778
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-28 22:58:46 +0200

    Updated all assembly versions.

--- Source/Reaper.SharpBattleNet.Framework.DiabloIIRealmServer/Properties/AssemblyInfo.cs
+++ Source/Reaper.SharpBattleNet.Framework.DiabloIIRealmServer/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("8f15165b-30fd-4655-a7c0-af08c53c6876")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.3.0")]
+[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]

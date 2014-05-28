19614d245657dba51f9230e578276461ce132125
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-28 22:42:57 +0200

    Updated assembly version information. Added reference to assembly to server application.

--- Source/Reaper.SharpBattleNet.Framework.DiabloIIRealmServer/Properties/AssemblyInfo.cs
+++ Source/Reaper.SharpBattleNet.Framework.DiabloIIRealmServer/Properties/AssemblyInfo.cs
@@ -2,35 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Reaper.SharpBattleNet.Framework.DiabloIIRealmServer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SharpBattle.net - Master Server Library")]
+[assembly: AssemblyDescription("Contains all the logic for hosting most if not all the services that is part of Battle.net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Reaper.SharpBattleNet.Framework.DiabloIIRealmServer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Reaper Interactive")]
+[assembly: AssemblyProduct("SharpBattle.net")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("8f15165b-30fd-4655-a7c0-af08c53c6876")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]

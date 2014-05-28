90ce24e7d7c9f365ab57a7ac66ff93e069097422
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-28 22:46:39 +0200

    Added initial library initialization stuff for D2RS.

---
+++ Source/Reaper.SharpBattleNet.Framework.DiabloIIRealmServer/Details/DiabloIIRealmServer.cs
@@ -0,0 +1,45 @@
+﻿namespace Reaper.SharpBattleNet.Framework.DiabloIIRealmServer.Details
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Nini;
+    using Nini.Config;
+    using Nini.Ini;
+    using Nini.Util;
+
+    using NLog;
+
+    using Reaper;
+    using Reaper.SharpBattleNet;
+    using Reaper.SharpBattleNet.Framework;
+    using Reaper.SharpBattleNet.Framework.DiabloIIRealmServer;
+
+    internal class DiabloIIRealmServer : IDiabloIIRealmServer
+    {
+        private readonly IConfigSource _configuration = null;
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public DiabloIIRealmServer(IConfigSource configuration)
+        {
+            _configuration = configuration;
+
+            return;
+        }
+
+        public void Start(string[] commandArguments)
+        {
+            return;
+        }
+
+        public void Stop()
+        {
+            return;
+        }
+    }
+}

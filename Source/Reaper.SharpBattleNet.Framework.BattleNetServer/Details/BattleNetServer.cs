aabbe5d4f09e84ae007ebbb0953b410f22b2c726
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-28 21:08:18 +0200

    Added Ninject module to handle the server framework. Added a runner class that is able to be called from both the console application and service application. Also, the basic server initialization code is there.

---
+++ Source/Reaper.SharpBattleNet.Framework.BattleNetServer/Details/BattleNetServer.cs
@@ -0,0 +1,29 @@
+﻿namespace Reaper.SharpBattleNet.Framework.BattleNetServer.Details
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Reaper;
+    using Reaper.SharpBattleNet;
+    using Reaper.SharpBattleNet.Framework;
+    using Reaper.SharpBattleNet.Framework.BattleNetServer;
+
+    internal class BattleNetServer : IBattleNetServer
+    {
+        public void Start(string[] commandArguments)
+        {
+            Console.WriteLine("In startup");
+            return;
+        }
+
+        public void Stop()
+        {
+            return;
+        }
+    }
+}

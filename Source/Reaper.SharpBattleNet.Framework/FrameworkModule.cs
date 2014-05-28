96b6d5ab81f44f88616f37469f25e9a1fa1fb650
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-28 21:14:06 +0200

    Added Ninject to framework library.

---
+++ Source/Reaper.SharpBattleNet.Framework/FrameworkModule.cs
@@ -0,0 +1,25 @@
+﻿namespace Reaper.SharpBattleNet.Framework
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Ninject;
+    using Ninject.Modules;
+
+    using Reaper;
+    using Reaper.SharpBattleNet;
+    using Reaper.SharpBattleNet.Framework;
+
+    public class FrameworkModule : NinjectModule
+    {
+        public override void Load()
+        {
+            return;
+        }
+    }
+}

1bd79b607ef90316aa4baf8aae6f6aceaf0892e9
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-29 14:16:58 +0200

    Updated assembly information.

--- Source/Reaper.SharpBattleNet.Framework.Networking/Properties/AssemblyInfo.cs
+++ Source/Reaper.SharpBattleNet.Framework.Networking/Properties/AssemblyInfo.cs
@@ -2,35 +2,15 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Reaper.SharpBattleNet.Framework.Networking")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SharpBattle.net Server - Network Framework")]
+[assembly: AssemblyDescription("Allows programs to host networking servers")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Reaper.SharpBattleNet.Framework.Networking")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Reaper Interactive")]
+[assembly: AssemblyProduct("SharpBattle.net")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("dafa7b7d-c510-4d5b-bf2b-ee7b9e0358a5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyFileVersion("0.0.4.0")]

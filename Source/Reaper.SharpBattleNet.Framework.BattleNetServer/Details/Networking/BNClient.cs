584ab7a6bafab696090b165320d130bc0418d20f
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-30 00:10:27 +0200

    Implemented initial BNS networking interfaces.

---
+++ Source/Reaper.SharpBattleNet.Framework.BattleNetServer/Details/Networking/BNClient.cs
@@ -0,0 +1,22 @@
+﻿namespace Reaper.SharpBattleNet.Framework.BattleNetServer.Details.Networking
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Ninject;
+    using Ninject.Modules;
+
+    using Reaper;
+    using Reaper.SharpBattleNet;
+    using Reaper.SharpBattleNet.Framework;
+    using Reaper.SharpBattleNet.Framework.Networking;
+
+    internal sealed class BNClient : BaseClient
+    {
+    }
+}
---
+++ Source/Reaper.SharpBattleNet.Framework.BattleNetServer/Details/Networking/BNClientFactory.cs
@@ -0,0 +1,26 @@
+﻿namespace Reaper.SharpBattleNet.Framework.BattleNetServer.Details.Networking
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Ninject;
+    using Ninject.Modules;
+
+    using Reaper;
+    using Reaper.SharpBattleNet;
+    using Reaper.SharpBattleNet.Framework;
+    using Reaper.SharpBattleNet.Framework.Networking;
+
+    internal sealed class BNClientFactory : IClientFactory
+    {
+        public IClient CreateClient()
+        {
+            return new BNClient();
+        }
+    }
+}

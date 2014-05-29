76325ff54e72b5ff28595f5d4d836e24ca8b7f2d
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-29 23:30:40 +0200

    Update D2RS to handle new networking infrastructure.

--- Source/Reaper.SharpBattleNet.Framework.DiabloIIRealmServer/Details/DiabloIIRealmServer.cs
+++ Source/Reaper.SharpBattleNet.Framework.DiabloIIRealmServer/Details/DiabloIIRealmServer.cs
@@ -18,27 +18,35 @@
     using Reaper;
     using Reaper.SharpBattleNet;
     using Reaper.SharpBattleNet.Framework;
+    using Reaper.SharpBattleNet.Framework.Networking;
     using Reaper.SharpBattleNet.Framework.DiabloIIRealmServer;
 
     internal sealed class DiabloIIRealmServer : IDiabloIIRealmServer
     {
         private readonly IConfigSource _configuration = null;
+        private readonly INetworkManager _networkManager = null;
+
         private readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
-        public DiabloIIRealmServer(IConfigSource configuration)
+        public DiabloIIRealmServer(IConfigSource configuration, INetworkManager networkManager)
         {
             _configuration = configuration;
+            _networkManager = networkManager;
 
             return;
         }
 
         public async Task Start(string[] commandArguments)
         {
+            await _networkManager.StartNetworking();
+
             return;
         }
 
         public async Task Stop()
         {
+            await _networkManager.StopNetworking();
+
             return;
         }
     }

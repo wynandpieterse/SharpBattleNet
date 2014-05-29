02dac91cf716da4c9fede3e405b1cc27a03b56ef
Wynand Pieterse <wpieterse1990@gmail.com>
2014-05-29 14:59:36 +0200

    Initial implementation of TCP clients for BNS. I have a feeling this is going to be refactored into the network library soon enough, when packet dispatching is more mature.

---
+++ Source/Reaper.SharpBattleNet.Framework.BattleNetServer/Details/Networking/TCPClient.cs
@@ -0,0 +1,81 @@
+﻿namespace Reaper.SharpBattleNet.Framework.BattleNetServer.Details.Networking
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Net;
+    using System.Net.Sockets;
+
+    using NLog;
+
+    using Reaper;
+    using Reaper.SharpBattleNet;
+    using Reaper.SharpBattleNet.Framework;
+    using Reaper.SharpBattleNet.Framework.Networking;
+
+    internal class TCPClient : ITCPClient
+    {
+        private byte[] _dataBuffer = null;
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public TCPClient()
+        {
+            _dataBuffer = new byte[2048];
+
+            return;
+        }
+
+        public Socket Socket { get; set; }
+        public CancellationTokenSource CancelToken { get; set; }
+
+        public void Accepted()
+        {
+            var socketAsyncArgs = new SocketAsyncEventArgs();
+
+            socketAsyncArgs.SetBuffer(_dataBuffer, 0, _dataBuffer.Length);
+            socketAsyncArgs.Completed += Process;
+
+            Socket.ReceiveAsync(socketAsyncArgs);
+
+            return;
+        }
+
+        private void Process(object sender, SocketAsyncEventArgs args)
+        {
+            try
+            {
+                if(true == CancelToken.IsCancellationRequested)
+                {
+                    return;
+                }
+                else
+                {
+                    var receivedBytes = args.BytesTransferred;
+
+                    if(0 != receivedBytes)
+                    {
+                        // Process the packet here
+                        _logger.Info("Got packet from {0} - {1}", Socket.RemoteEndPoint, _dataBuffer.ToString());
+
+                        Socket.ReceiveAsync(args);
+                    }
+                    else
+                    {
+                        Socket.Close();
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.WarnException("Failed during receive", ex);
+                return;
+            }
+
+            return;
+        }
+    }
+}
---
+++ Source/Reaper.SharpBattleNet.Framework.BattleNetServer/Details/Networking/TCPClientFactory.cs
@@ -0,0 +1,25 @@
+﻿namespace Reaper.SharpBattleNet.Framework.BattleNetServer.Details.Networking
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Net;
+    using System.Net.Sockets;
+
+    using Reaper;
+    using Reaper.SharpBattleNet;
+    using Reaper.SharpBattleNet.Framework;
+    using Reaper.SharpBattleNet.Framework.Networking;
+
+    internal class TCPClientFactory : ITCPClientFactory
+    {
+        public async Task<ITCPClient> CreateClient()
+        {
+            return new TCPClient();
+        }
+    }
+}

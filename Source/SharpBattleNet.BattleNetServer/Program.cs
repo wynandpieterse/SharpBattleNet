f2b1641feb07d2eebd5e1d6a1fdef36ffb2e310d
Wynand Pieterse <wpieterse1990@gmail.com>
2014-08-07 21:04:52 +0200

    Added nice logo. More error handeling stuff.

--- Source/SharpBattleNet.BattleNetServer/Program.cs
+++ Source/SharpBattleNet.BattleNetServer/Program.cs
@@ -15,18 +15,36 @@ namespace SharpBattleNet.Servers.BattleNetServer
     using Ninject;
     using SharpBattleNet.Framework.Networking;
     using SharpBattleNet.Server.BattleNetServer;
+    using System.Diagnostics;
 
     internal static class Program
     {
         private static IKernel _injectionKernel = null;
 
+        private static void PrintHeader(Assembly currentAssembly)
+        {
+            Console.WriteLine(@"    _  _   ____        _   _   _         _   _      _    ");
+            Console.WriteLine(@"  _| || |_| __ )  __ _| |_| |_| | ___   | \ | | ___| |_  ");
+            Console.WriteLine(@" |_  .. _ |  _ \ / _` | __| __| |/ _ \  |  \| |/ _ \ __| ");
+            Console.WriteLine(@" |_      _| |_) | (_| | |_| |_| |  __/ _| |\  |  __/ |_  ");
+            Console.WriteLine(@"   |_||_| |____/ \__,_|\__|\__|_|\___(_)_ | \_|\___|\__| ");
+
+            Console.WriteLine();
+            Console.WriteLine("{0} - {1}", currentAssembly.GetAssemblyTitle(), currentAssembly.GetAssemblyFileVersion());
+            Console.WriteLine();
+
+            return;
+        }
+
         private static void Start(string[] commandArguments)
         {
             var currentAssembly = Assembly.GetExecutingAssembly();
 
-            Console.Title = String.Format("{0} - {1}", currentAssembly.GetAssemblyTitle(), currentAssembly.GetAssemblyFileVersion());
+            Console.Title = String.Format("{ 0} - {1}", currentAssembly.GetAssemblyTitle(), currentAssembly.GetAssemblyFileVersion());
             Console.WindowWidth = 120;
             Console.WindowHeight = 40;
+
+            PrintHeader(currentAssembly);
 
             _injectionKernel = new StandardKernel(new FrameworkModule("BattleNetServer"), new NetworkModule(), new BattleNetServerModule());
 
@@ -47,12 +65,12 @@ namespace SharpBattleNet.Servers.BattleNetServer
 
             Console.WriteLine("Press ENTER to continue...");
 
-            while(false == enterPressed)
+            while (false == enterPressed)
             {
-                if(true == Console.KeyAvailable)
+                if (true == Console.KeyAvailable)
                 {
                     lastKey = Console.ReadKey(true);
-                    if(lastKey.Key == ConsoleKey.Enter)
+                    if (lastKey.Key == ConsoleKey.Enter)
                     {
                         enterPressed = true;
                     }
@@ -62,11 +80,44 @@ namespace SharpBattleNet.Servers.BattleNetServer
             return;
         }
 
-        private static void Main(string[] args)
+        [Conditional("DEBUG")]
+        private static void DebugMain(string[] args)
         {
             Start(args);
             Pause();
             Stop();
+
+            return;
+        }
+
+        [Conditional("RELEASE")]
+        private static void ReleaseMain(string[] args)
+        {
+            try
+            {
+                Start(args);
+                Pause();
+                Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Internal server error:");
+                Console.WriteLine(" - {0}", ex.Message);
+
+                if (null != ex.InnerException)
+                {
+                    Console.WriteLine(" - {0}", ex.InnerException.Message);
+                }
+            }
+
+            return;
+        }
+
+        private static void Main(string[] args)
+        {
+            DebugMain(args);
+            ReleaseMain(args);
+
             return;
         }
     }

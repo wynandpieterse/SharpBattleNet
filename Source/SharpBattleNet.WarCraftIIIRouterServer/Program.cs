01bced4e82f42de58bb4580db03a6f1fd4a5e3b5
Wynand Pieterse <wpieterse1990@gmail.com>
2014-08-12 20:46:19 +0200

    Added base package restore and initial program layout copied from master server.

--- Source/SharpBattleNet.WarCraftIIIRouterServer/Program.cs
+++ Source/SharpBattleNet.WarCraftIIIRouterServer/Program.cs
@@ -1,15 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region Header
+//
+//    _  _   ____        _   _   _         _   _      _   
+//  _| || |_| __ )  __ _| |_| |_| | ___   | \ | | ___| |_ 
+// |_  .. _ |  _ \ / _` | __| __| |/ _ \  |  \| |/ _ \ __|
+// |_      _| |_) | (_| | |_| |_| |  __/_ | |\  |  __/ |_ 
+//   |_||_| |____/ \__,_|\__|\__|_|\___(_)_ | \_|\___|\__|
+//
+// The MIT License
+// 
+// Copyright(c) 2014 Wynand Pieters. https://github.com/wpieterse/SharpBattleNet
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#endregion
 
 namespace SharpBattleNet.WarCraftIIIRouterServer
 {
-    class Program
+    #region Usings
+    using System;
+    using Ninject;
+    using SharpBattleNet.Framework;
+    using SharpBattleNet.Server.MasterServer;
+    using SharpBattleNet.Framework.Networking;
+    #endregion
+
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static int Main(string[] args)
         {
+            FrameworkProgram program = new FrameworkProgram();
+
+            program.Configure = kernel =>
+                {
+                    kernel.Load<MasterServerModule>();
+                    kernel.Load<NetworkModule>();
+
+                    return "MasterServer";
+                };
+
+            return program.Run(args);
         }
     }
 }
+

65653d6c1222a42e29af425db9567c92057efecb
Wynand Pieterse <wpieterse1990@gmail.com>
2014-08-12 21:18:23 +0200

    Added projects for D2GS.

---
+++ Source/SharpBattleNet.DiabloIIGameServer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpBattleNet.DiabloIIGameServer
+{
+    public class Class1
+    {
+    }
+}

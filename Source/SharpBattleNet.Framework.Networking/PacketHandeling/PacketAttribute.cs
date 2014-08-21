f75bbab47ba4b240c8ffd4a58d34b0187002e202
Wynand Pieterse <wpieterse1990@gmail.com>
2014-08-21 16:35:28 +0200

    Initial stab at packet dispatchers and handlers. Most of the interfaces are in now. No implementation is yet available.

---
+++ Source/SharpBattleNet.Framework.Networking/PacketHandeling/PacketAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpBattleNet.Framework.Networking.PacketHandeling
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public sealed class PacketAttribute : Attribute
+    {
+        public PacketAttribute()
+        {
+            return;
+        }
+    }
+}
---
+++ Source/SharpBattleNet.Server.MasterServer/TestPacket.cs
@@ -0,0 +1,38 @@
+﻿using SharpBattleNet.Framework.Networking.PacketHandeling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpBattleNet.Server.MasterServer
+{
+    [Packet]
+    public sealed class TestPacket : IPacket
+    {
+        internal class Serializer : IPacketSerializer
+        {
+
+        }
+
+        internal class Executor : IPacketExecutor
+        {
+
+        }
+
+        public PacketDetails Details
+        {
+            get
+            {
+                PacketDetails details = new PacketDetails();
+
+                details.Program = 0;
+                details.MessageID = 1;
+                details.Executor = new Executor();
+                details.Serializer = new Serializer();
+
+                return details;
+            }
+        }
+    }
+}

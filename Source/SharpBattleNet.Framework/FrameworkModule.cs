87176a9f268fdedc76fe27060d460b6a0709bd08
Wynand Pieterse <wpieterse1990@gmail.com>
2014-08-30 11:22:05 +0200

    Removed test data from master program output.

--- Source/SharpBattleNet.Framework/FrameworkModule.cs
+++ Source/SharpBattleNet.Framework/FrameworkModule.cs
@@ -242,7 +242,7 @@ namespace SharpBattleNet.Framework
                 config.AddTarget("file", fileTarget);
 
                 fileTarget.FileName = logFilename;
-                fileTarget.Layout = @"${processtime} ${threadid} ${threadname} ${level} ${logger} ${message}";
+                fileTarget.Layout = @"${processtime} ${threadid} ${level} ${logger} ${message}";
 
                 var fileRule = new LoggingRule("*", LogLevel.Debug, fileTarget);
                 config.LoggingRules.Add(fileRule);

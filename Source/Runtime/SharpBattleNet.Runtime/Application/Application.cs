87a1b2d6e1021ba46a0b1aeab7e79fdf8258d763
Wynand Pieterse <wpieterse1990@gmail.com>
2014-09-12 19:28:54 +0200

    Initial layout of new application framework is there.

---
+++ Source/Runtime/SharpBattleNet.Runtime/Application/Application.cs
@@ -0,0 +1,27 @@
+﻿using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpBattleNet.Runtime.Application
+{
+    public class Application
+    {
+        public Application(string name, string[] arguments)
+        {
+            return;
+        }
+
+        public void AddDependencyModule(NinjectModule module)
+        {
+            return;
+        }
+
+        public int Run()
+        {
+            return 0;
+        }
+    }
+}

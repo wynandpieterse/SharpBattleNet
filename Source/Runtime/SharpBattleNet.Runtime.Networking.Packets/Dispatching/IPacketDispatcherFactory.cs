9d83a95dc96ab00ad85da983711797dfc4e9175e
Wynand Pieterse <wpieterse1990@gmail.com>
2014-10-15 17:29:29 +0200

    Updated packet dispatcher factory.

--- Source/Runtime/SharpBattleNet.Runtime.Networking.Packets/Dispatching/IPacketDispatcherFactory.cs
+++ Source/Runtime/SharpBattleNet.Runtime.Networking.Packets/Dispatching/IPacketDispatcherFactory.cs
@@ -1,13 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region Header
+//
+//    _  _   ____        _   _   _         _   _      _   
+//  _| || |_| __ )  __ _| |_| |_| | ___   | \ | | ___| |_ 
+// |_  .. _ |  _ \ / _` | __| __| |/ _ \  |  \| |/ _ \ __|
+// |_      _| |_) | (_| | |_| |_| |  __/_ | |\  |  __/ |_ 
+//   |_||_| |____/ \__,_|\__|\__|_|\___(_)_ | \_|\___|\__|
+//
+// The MIT License
+// 
+// Copyright(c) 2014 Wynand Pieters. https://github.com/wpieterse/SharpBattleNet
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#endregion
 
 namespace SharpBattleNet.Runtime.Networking.PacketHandeling.Dispatching
 {
+    #region Using
+    using System;
+    #endregion
+
+    /// <summary>
+    /// Factory for creating new packet dispatchers. Used by user application to create a new packet dispatcher that will be used for the certain connection from a client.
+    /// </summary>
     public interface IPacketDispatcherFactory
     {
+        /// <summary>
+        /// Creates a new packet dispatcher that listens for packets that contains the specified programs ID.
+        /// </summary>
+        /// <param name="program">The program ID to make sure the packets are routed to the correct handlers.</param>
+        /// <returns>New packet dispatcher that can be used by the users application to handle packets.</returns>
         IPacketDispatcher Create(uint program);
     }
 }

79429f69f33e1e4d492c2ffa530f13a41370c9ec
Wynand Pieterse <wpieterse1990@gmail.com>
2014-10-15 17:31:12 +0200

    Updated dispatcher state enumeration.

--- Source/Runtime/SharpBattleNet.Runtime.Networking.Packets/Dispatching/Details/PacketDispatcherState.cs
+++ Source/Runtime/SharpBattleNet.Runtime.Networking.Packets/Dispatching/Details/PacketDispatcherState.cs
@@ -1,15 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿#region Header
+//
+//    _  _   ____        _   _   _         _   _      _   
+//  _| || |_| __ )  __ _| |_| |_| | ___   | \ | | ___| |_ 
+// |_  .. _ |  _ \ / _` | __| __| |/ _ \  |  \| |/ _ \ __|
+// |_      _| |_) | (_| | |_| |_| |  __/_ | |\  |  __/ |_ 
+//   |_||_| |____/ \__,_|\__|\__|_|\___(_)_ | \_|\___|\__|
+//
+// The MIT License
+// 
+// Copyright(c) 2014 Wynand Pieters. https://github.com/wpieterse/SharpBattleNet
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#endregion
 
 namespace SharpBattleNet.Runtime.Networking.PacketHandeling.Dispatching.Details
 {
+    #region Using
+    using System;
+    #endregion
+
+    /// <summary>
+    /// The state that the current packet dispatcher is in. Used for different code flows inside the packet dispatcher.
+    /// </summary>
     public enum PacketDispatcherState : byte
     {
+        /// <summary>
+        /// The packet dispatcher is not yet initialized.
+        /// </summary>
         Uninitialized = 0,
+
+        /// <summary>
+        /// The packet dispatcher is busy processing the header of a packet.
+        /// </summary>
         Header = 1,
+        
+        /// <summary>
+        /// The packet dispatcher is busy processing the body of a packet.
+        /// </summary>
         Data = 2
     }
 }

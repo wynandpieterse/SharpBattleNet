860766629504765870b31e8413dd6d38bd2ddcb5
Wynand Pieterse <wpieterse1990@gmail.com>
2014-10-15 14:26:46 +0200

    Updated all using statements to include new library code.

--- Source/Runtime/SharpBattleNet.Runtime/Utilities/Logging/LogProvider.cs
+++ Source/Runtime/SharpBattleNet.Runtime/Utilities/Logging/LogProvider.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SharpBattleNet.Runtime.Utilities.Logging.Providers;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
--- Source/Runtime/SharpBattleNet.Runtime/Utilities/Logging/Providers/EntLibLogProvider.cs
+++ Source/Runtime/SharpBattleNet.Runtime/Utilities/Logging/Providers/EntLibLogProvider.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 

057a950dc2af8edd2d54d53f61bd9d8d8dae13c1
Wynand Pieterse <wpieterse1990@gmail.com>
2014-10-15 18:58:10 +0200

    Removed all comments from buffer pool code. Also, added headers and stuff to buffer pool code.

--- Source/Runtime/SharpBattleNet.Runtime/Utilities/BufferPool/BufferInterfaces.cs
+++ Source/Runtime/SharpBattleNet.Runtime/Utilities/BufferPool/BufferInterfaces.cs
@@ -1,149 +1,73 @@
-﻿#region License
-/*
-   Copyright 2011 Sunny Ahuwanya (www.ahuwanya.net)
+﻿#region Header
+//
+//    _  _   ____        _   _   _         _   _      _   
+//  _| || |_| __ )  __ _| |_| |_| | ___   | \ | | ___| |_ 
+// |_  .. _ |  _ \ / _` | __| __| |/ _ \  |  \| |/ _ \ __|
+// |_      _| |_) | (_| | |_| |_| |  __/_ | |\  |  __/ |_ 
+//   |_||_| |____/ \__,_|\__|\__|_|\___(_)_ | \_|\___|\__|
+//
+// The MIT License
+// 
+// Copyright(c) 2014 Wynand Pieters. https://github.com/wpieterse/SharpBattleNet
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
 #endregion
 
 namespace SharpBattleNet.Runtime.Utilities.BufferPool
 {
+    #region Usings
     using System;
+    using System.Collections;
     using System.Collections.Generic;
+    #endregion
 
-    /// <summary>
-    /// Represents a buffer for asynchronous socket operations and defines methods to copy data into and out of the buffer
-    /// </summary>
     public interface IBuffer : IDisposable
     {
-        /// <summary>
-        /// Gets the total size of the buffer, in bytes.
-        /// </summary>
         long Size { get; }
-
-        /// <summary>
-        /// Gets a value indicating whether the buffer is disposed.
-        /// </summary>
         bool IsDisposed { get; }
-
-        /// <summary>
-        /// Gets the number of segments in the buffer.
-        /// </summary>
         int SegmentCount { get; }
-
-        /// <summary>
-        /// Gets buffer segments that can be passed on to an asynchronous socket operation.
-        /// </summary>
-        /// <returns>A list of ArraySegments(of Byte) containing buffer segments.</returns>
+        
         IList<ArraySegment<byte>> GetSegments();
-
-        /// <summary>
-        /// Gets buffer segments that can be passed on to an asynchronous socket operation.
-        /// </summary>
-        /// <param name="length">Total length of segments.</param>
-        /// <returns>A list of ArraySegments(of Byte) containing buffer segments.</returns>
         IList<ArraySegment<byte>> GetSegments(long length);
-
-        /// <summary>
-        /// Gets buffer segments that can be passed on to an asynchronous socket operation.
-        /// </summary>
-        /// <param name="offset">Offset in the buffer where segments start.</param>
-        /// <param name="length">Total length of segments.</param>
-        /// <returns>A list of ArraySegments(of Byte) containing buffer segments.</returns>
         IList<ArraySegment<byte>> GetSegments(long offset, long length);
 
-        /// <summary>
-        /// Copies data from the buffer into a byte array.
-        /// </summary>
-        /// <param name="destinationArray">The one-dimensional byte array which receives the data.</param>
-        /// <remarks>The size of the buffer must be less than or equal to the destinationArray length.</remarks>
         void CopyTo(byte[] destinationArray);
-
-        /// <summary>
-        /// Copies data from the buffer to a byte array
-        /// </summary>
-        /// <param name="destinationArray">The one-dimensional byte array which receives the data.</param>
-        /// <param name="destinationIndex">The index in the destinationArray at which storing begins.</param>
-        /// <param name="length">The number of bytes to copy.</param>
         void CopyTo(byte[] destinationArray, long destinationIndex, long length);
 
-        /// <summary>
-        /// Copies data from a byte array to the buffer.
-        /// </summary>
-        /// <param name="sourceArray">The one-dimensional byte array that contains the data.</param>
-        /// <remarks>The length of the sourceArray must be less than or equal to the buffer size.</remarks>
         void FillWith(byte[] sourceArray);
-
-        /// <summary>
-        /// Copies data from a byte array to the buffer.
-        /// </summary>
-        /// <param name="sourceArray">The one-dimensional byte array that contains the data.</param>
-        /// <param name="sourceIndex">The index in the sourceArray at which copying begins.</param>
-        /// <param name="length">The number of bytes to copy.</param>
         void FillWith(byte[] sourceArray, long sourceIndex, long length);
 
-        /// <summary>
-        /// Copies data from a byte array to the buffer.
-        /// </summary>
-        /// <param name="sourceArray">The one-dimensional byte array that contains the data.</param>
-        /// <remarks>The length of the sourceArray must be less than or equal to the buffer size.</remarks>
         [Obsolete("Use the FillWith method instead -- this method will be removed in a later version",true)]
         void CopyFrom(byte[] sourceArray);
 
-        /// <summary>
-        /// Copies data from a byte array to the buffer.
-        /// </summary>
-        /// <param name="sourceArray">The one-dimensional byte array that contains the data.</param>
-        /// <param name="sourceIndex">The index in the sourceArray at which copying begins.</param>
-        /// <param name="length">The number of bytes to copy.</param>
         [Obsolete("Use the FillWith method instead -- this method will be removed in a later version", true)]
         void CopyFrom(byte[] sourceArray, long sourceIndex, long length);
     }
 
-    /// <summary>
-    /// Represents a pool of IBuffer objects 
-    /// </summary>
     public interface IBufferPool
     {
-        /// <summary>
-        /// Gets the initial number of slabs created
-        /// </summary>
         int InitialSlabs { get; }
-
-        /// <summary>
-        /// Gets the additional number of slabs to be created at a time
-        /// </summary>
         int SubsequentSlabs { get; }
-
-        /// <summary>
-        /// Gets the slab size, in bytes
-        /// </summary>
         long SlabSize { get; }
 
-        /// <summary>
-        /// Creates a buffer of the specified size
-        /// </summary>
-        /// <param name="size">Buffer size, in bytes</param>
-        /// <returns>IBuffer object of requested size</returns>
         IBuffer GetBuffer(long size);
-
-        /// <summary>
-        /// Creates a buffer of the specified size, filled with the contents of a specified byte array
-        /// </summary>
-        /// <param name="size">Buffer size, in bytes</param>
-        /// <param name="filledWith">Byte array to copy to buffer</param>
-        /// <returns>IBuffer object of requested size</returns>
         IBuffer GetBuffer(long size, byte[] filledWith);
-
     }
 }
--- Source/Runtime/SharpBattleNet.Runtime/Utilities/BufferPool/MemoryInterfaces.cs
+++ Source/Runtime/SharpBattleNet.Runtime/Utilities/BufferPool/MemoryInterfaces.cs
@@ -1,95 +1,57 @@
-﻿#region License
-/*
-   Copyright 2011 Sunny Ahuwanya (www.ahuwanya.net)
+﻿#region Header
+//
+//    _  _   ____        _   _   _         _   _      _   
+//  _| || |_| __ )  __ _| |_| |_| | ___   | \ | | ___| |_ 
+// |_  .. _ |  _ \ / _` | __| __| |/ _ \  |  \| |/ _ \ __|
+// |_      _| |_) | (_| | |_| |_| |  __/_ | |\  |  __/ |_ 
+//   |_||_| |____/ \__,_|\__|\__|_|\___(_)_ | \_|\___|\__|
+//
+// The MIT License
+// 
+// Copyright(c) 2014 Wynand Pieters. https://github.com/wpieterse/SharpBattleNet
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
 #endregion
 
 namespace SharpBattleNet.Runtime.Utilities.BufferPool
 {
-    /// <summary>
-    /// Represents a block of memory
-    /// </summary>
+    #region Usings
+    using System;
+    #endregion
+
     internal interface IMemoryBlock
     {
-        /// <summary>
-        /// Gets the offset in the slab where the memory block begins.
-        /// </summary>
         long StartLocation { get; }
-
-        /// <summary>
-        /// Gets the offset in the slab where the memory block ends.
-        /// </summary>
-        /// <remarks> EndLocation = StartLocation + Length - 1</remarks>
         long EndLocation { get; }
-
-        /// <summary>
-        /// Gets the length of the memory block, in bytes.
-        /// </summary>
-        /// <remarks> Length = EndLocation - StartLocation + 1</remarks>
         long Length { get; }
-
-        /// <summary>
-        /// Gets the containing slab
-        /// </summary>
         IMemorySlab Slab { get; }
-
     }
 
-    /// <summary>
-    /// Represents a memory slab, from which memory blocks are allocated
-    /// </summary>
     internal interface IMemorySlab
     {
-        /// <summary>
-        /// Gets the size, in bytes, of the memory slab.
-        /// </summary>
         long Size { get; }
-
-        /// <summary>
-        /// Gets the last known largest unallocated contiguous block size.
-        /// </summary>
         long LargestFreeBlockSize { get; }
-
-        /// <summary>
-        /// Gets the underlying byte array that is wrapped by the memory slab
-        /// </summary>
         byte[] Array { get; }
 
-        /// <summary>
-        /// Attempts to allocate a memory block of a specified length.
-        /// </summary>
-        /// <param name="length">Length, in bytes, of memory block</param>
-        /// <param name="allocatedBlock">Allocated memory block</param>
-        /// <returns>True, if memory block was allocated. False, if otherwise</returns>
         bool TryAllocate(long length, out IMemoryBlock allocatedBlock);
-
-        /// <summary>
-        /// Attempts to allocate a memory block of length between minLength and maxLength (both inclusive)
-        /// </summary>
-        /// <param name="minLength">The minimum acceptable length</param>
-        /// <param name="maxLength">The maximum acceptable length</param>
-        /// <param name="allocatedBlock">Allocated memory block</param>
-        /// <returns>Length of allocated block if successful, zero otherwise</returns>
-        /// <remarks>This overload is useful when multiple threads are concurrently working on the slab and the caller wants to allocate a block up to a desired size</remarks>
         long TryAllocate(long minLength, long maxLength, out IMemoryBlock allocatedBlock);
-
-        /// <summary>
-        /// Frees an allocated memory block.
-        /// </summary>
-        /// <param name="allocatedBlock">Allocated memory block to be freed</param>
         void Free(IMemoryBlock allocatedBlock);
-
     }
 }

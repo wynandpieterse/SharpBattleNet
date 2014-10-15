df60410a429b6116c3c3e0c5d789e1fbd234d7ef
Wynand Pieterse <wpieterse1990@gmail.com>
2014-10-15 17:36:09 +0200

    Updated packet dispatcher implementation.

--- Source/Runtime/SharpBattleNet.Runtime.Networking.Packets/Dispatching/Details/PacketDispatcher.cs
+++ Source/Runtime/SharpBattleNet.Runtime.Networking.Packets/Dispatching/Details/PacketDispatcher.cs
@@ -1,19 +1,57 @@
-﻿using SharpBattleNet.Runtime.Utilities.Debugging;
-using System;
-using System.Collections.Generic;
-using System.Collections.Concurrent;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SharpBattleNet.Runtime.Networking.Connection;
-using SharpBattleNet.Runtime.Utilities.BufferPool;
-using System.Reflection;
-using SharpBattleNet.Runtime.Utilities.Extensions;
-using SharpBattleNet.Runtime.Networking.PacketHandeling.Execution;
-using SharpBattleNet.Runtime.Networking.PacketHandeling.Serialization;
+﻿#region Header
+//
+//    _  _   ____        _   _   _         _   _      _   
+//  _| || |_| __ )  __ _| |_| |_| | ___   | \ | | ___| |_ 
+// |_  .. _ |  _ \ / _` | __| __| |/ _ \  |  \| |/ _ \ __|
+// |_      _| |_) | (_| | |_| |_| |  __/_ | |\  |  __/ |_ 
+//   |_||_| |____/ \__,_|\__|\__|_|\___(_)_ | \_|\___|\__|
+//
+// The MIT License
+// 
+// Copyright(c) 2014 Wynand Pieters. https://github.com/wpieterse/SharpBattleNet
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+#endregion
 
 namespace SharpBattleNet.Runtime.Networking.PacketHandeling.Dispatching.Details
 {
+    #region Using
+    using System;
+    using System.Collections;
+    using System.Collections.Concurrent;
+
+    using SharpBattleNet;
+    using SharpBattleNet.Runtime;
+    using SharpBattleNet.Runtime.Utilities;
+    using SharpBattleNet.Runtime.Utilities.BufferPool;
+    using SharpBattleNet.Runtime.Utilities.Debugging;
+    using SharpBattleNet.Runtime.Networking;
+    using SharpBattleNet.Runtime.Networking.Connection;
+    using SharpBattleNet.Runtime.Networking.PacketHandeling;
+    using SharpBattleNet.Runtime.Networking.PacketHandeling.Execution;
+    using SharpBattleNet.Runtime.Networking.PacketHandeling.Serialization;
+    #endregion
+
+    /// <summary>
+    /// Implements <see cref="IPacketDispatcher"/>.
+    /// </summary>
     internal sealed class PacketDispatcher : IPacketDispatcher
     {
         private readonly uint _program = 0;
@@ -24,6 +62,11 @@ namespace SharpBattleNet.Runtime.Networking.PacketHandeling.Dispatching.Details
         private ConcurrentDictionary<Tuple<int, int>, IPacketSerializer> _packetSerializers = null;
         private ConcurrentDictionary<Tuple<int, int>, IPacketExecutor> _packetExecutors = null;
 
+        /// <summary>
+        /// Constructs the packet dispatcher and initializes it into an empty state. The user application must call initialize to start the engine.
+        /// </summary>
+        /// <param name="program">The program ID uniquely specified which packets the dispatcher will handle for the user application.</param>
+        /// <param name="headerExecutor">The executor that will handle the packets when the dispatcher is in header execution mode.</param>
         public PacketDispatcher(uint program, IPacketHeaderExecutor headerExecutor)
         {
             Guard.AgainstNull(headerExecutor);
@@ -37,6 +80,11 @@ namespace SharpBattleNet.Runtime.Networking.PacketHandeling.Dispatching.Details
             return;
         }
 
+        /// <summary>
+        /// Initializes the packet dispatcher. This incorporates the connection into the packet dispatcher so that it knows who to pass through to the handlers
+        /// when new messages arive.
+        /// </summary>
+        /// <param name="connection">The connection that owns this packet dispatcher.</param>
         public void Initialize(IConnection connection)
         {
             Guard.AgainstNull(connection);
@@ -47,7 +95,12 @@ namespace SharpBattleNet.Runtime.Networking.PacketHandeling.Dispatching.Details
             return;
         }
 
-        public void Process(IBuffer buffer)
+        /// <summary>
+        /// Processes a new messages that was received on the network stream. If there is enough data available, passes the packet through the process until the
+        /// packet is handled inside user code.
+        /// </summary>
+        /// <param name="recievedBuffer">The data that was received from the network connection. Do not dispose, will be handled by the network subsystem.</param>
+        public void Process(IBuffer recievedBuffer)
         {
 
 
